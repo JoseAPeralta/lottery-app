ba018a9fcbeee5b5baea15872e3490ce118ae0c3
Jose A Peralta <japc1802@hotmail.com>
2023-01-06 23:01:38 -0500

    add vite-plugin-restart

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,16 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import removeConsole from 'vite-plugin-remove-console';
 import { qrcode } from 'vite-plugin-qrcode';
-
+import ViteRestart from 'vite-plugin-restart';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), removeConsole(), qrcode()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    removeConsole(),
+    qrcode(),
+    ViteRestart({
+      restart: ['vite.config.[jt]s', 'tsconfig.json', 'tsconfig.node.json', 'package.json'],
+    }),
+  ],
 });
