2d00bb7592438ac22acd84236e1e834984e3687f
Jose A Peralta <japc1802@hotmail.com>
2023-01-06 23:37:00 -0500

    remove vite-plugin-restart add chakraui add theme config file to chakraui remove vite starter code

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  red: {
+    900: '#1a365d',
+    800: 'blue',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
+export default theme
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import removeConsole from 'vite-plugin-remove-console';
 import { qrcode } from 'vite-plugin-qrcode';
-import ViteRestart from 'vite-plugin-restart';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,8 +10,5 @@ export default defineConfig({
     tsconfigPaths(),
     removeConsole(),
     qrcode(),
-    ViteRestart({
-      restart: ['vite.config.[jt]s', 'tsconfig.json', 'tsconfig.node.json', 'package.json'],
-    }),
   ],
 });
