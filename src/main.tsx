72bccf79040729d1f34f9d47a1f7f2c6305813dd
Jose A Peralta <japc1802@hotmail.com>
2023-01-07 00:34:29 -0500

    refactor main.tsx to remove import warning in ReactDOM import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
 import '@fontsource/roboto';
