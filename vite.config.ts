94f4bdf18f37e9240d0f305dbae3e27217756192
Jose A Peralta <japc1802@hotmail.com>
2023-02-23 10:42:11 -0500

    add vite-plugin-pwa create webmanifest in manifest.ts include manifest.ts in tsconfig.json and tsconfig.node.json add favicon add icons add maskable icons modify page title to "Loteria de Panama"

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,24 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import removeConsole from 'vite-plugin-remove-console';
+import { VitePWA } from 'vite-plugin-pwa';
 import { qrcode } from 'vite-plugin-qrcode';
+import { manifest } from './src/manifest';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), removeConsole(), qrcode()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    removeConsole(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: manifest,
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),
+    qrcode(),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
