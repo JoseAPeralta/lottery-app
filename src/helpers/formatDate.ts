1d043502633536c445d47b42da3cfefa06ed129d
Jose A Peralta <japc1802@hotmail.com>
2023-02-28 10:29:30 -0500

    add test to format date helper

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,4 +1,4 @@
-export const DateToSpanish = (date: Date) => {
+export const DateToSpanish = (date: Date | string | number) => {
   const myDate = new Date(date);
   return myDate.toLocaleDateString('es-ES', { year: 'numeric', month: 'long', day: '2-digit' });
 };
